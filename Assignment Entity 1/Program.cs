57946ab46a5958824c1e5186d3a61067122b6ab8
Hesham Fathy <heshamfathykamal@gmail.com>
2024-08-26 19:27:12 +0300

    edit

--- Assignment Entity 1/Program.cs
+++ Assignment Entity 1/Program.cs
@@ -5,6 +5,51 @@
         static void Main()
         {
 
+            #region create 
+
+            using (var context = new itiDbContext())
+            {
+                var student = new Student { FName = "John", LName = "Doe", Age = 20, Dep_Id = 1 };
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+
+
+            #endregion
+
+            #region REad
+
+            using (var context = new itiDbContext())
+            {
+                var students = context.Students.ToList();
+            }
+
+
+
+            #endregion
+
+            #region update
+            using (var context = new itiDbContext())
+            {
+                var student = context.Students.First();
+                student.LName = "Smith";
+                context.SaveChanges();
+            }
+
+
+            #endregion
+
+            #region Delete
+
+            using (var context = new itiDbContext())
+            {
+                var student = context.Students.First();
+                context.Students.Remove(student);
+                context.SaveChanges();
+            }
+
+            #endregion
+
         }
     }
 }
